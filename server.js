01514b49379e92c37ddd8c39cd7f25d9e4c4eb55
Joshua McNeel <joshmcneel@gmail.com>
2025-07-28 16:46:15 -0400

    feat: course project results

--- server.js
+++ server.js
@@ -3,22 +3,28 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
+const cors = require('cors');
+app.use(cors());
+const bcrypt = require('bcrypt');
 fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
-
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash);
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+        console.log(res); // true
+    });
+});
 //END_ASYNC
 
 //START_SYNC
-
-
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result); // true
 //END_SYNC
 
 
